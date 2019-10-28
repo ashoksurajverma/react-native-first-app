3569ee18f08b6808552d83916090b1457e5d50d3
Suraj Verma <ashoksurajverma@gmail.com>
2019-10-28 23:53:35 +0530

    set the reset button functinality

--- src/screens/StartGameScreen.js
+++ src/screens/StartGameScreen.js
@@ -1,20 +1,42 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, StyleSheet, TextInput, Button, Keyboard } from 'react-native';
 
 import Card from '../components/Card';
 import Colors from '../constants/color';
 import Input from '../components/Input';
 
 const StartGameScreen = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const numberInputHandler = inputText => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ''));
+  }
+
+  const resetInputHandler = () => {
+    setEnteredValue('');
+    setConfirmed(false);
+  }
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
       <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
-        <Input style={styles.input} onBlurOnSubmit autoCapitalize="none" autoCorrect={false} keyboardType="number-pad" maxLength={2}/>
+        <Input
+          style={styles.input}
+          onBlurOnSubmit
+          autoCapitalize="none"
+          autoCorrect={false}
+          keyboardType="number-pad"
+          maxLength={2}
+          onChangeText={numberInputHandler}
+          value={enteredValue}
+        />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Reset" onPress={() => {}} color={Colors.accent} />
+            <Button title="Reset" onPress={resetInputHandler} />
           </View>
           <View style={styles.button}>
             <Button title="Confirm" onPress={() => {}} color={Colors.primary} />
